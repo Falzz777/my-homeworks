a413bbc8dab4180b7e9aae71166a01a2bd601d14
Falzz777 <falzz@yandex.ru>
2025-11-04 20:38:50 +0300

    homework-1/v2

--- homework-1/main.go
+++ homework-1/main.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	const usdInEur float64 = 0.87
 	const usdInRub float64 = 80.9
-	const eurInRub float64 = usdInEur * usdInRub
+	const eurInRub float64 = usdInEur / usdInRub
 	fmt.Println(eurInRub)
 }
