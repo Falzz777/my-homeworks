1eb165210ed8c279c118756a3f5c2e1fd05faf4d
Falzz777 <falzz@yandex.ru>
2025-11-06 18:41:57 +0300

    Add homework-2

---
+++ homework-2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+
+}
+
+func getUserInput() (float64) {
+	var userInput float64
+	fmt.Print("Введите ваше значение"
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculatUserInfo (int, str, str) (result float64) {
+
+}
