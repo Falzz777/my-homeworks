12d915269747546000a43f1843325d1544d2d989
Falzz777 <falzz@yandex.ru>
2025-11-09 22:22:25 +0300

    Add homework 3

--- homework-2/main.go
+++ homework-2/main.go
@@ -1,21 +1,53 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"reflect"
 )
 
 func main() {
-
+	for {
+		amount, currency := getUserInput()
+		err := checkError(amount, currency)
+		if err != nil {
+			fmt.Println("Введите правильные значения!!!")
+			continue
+		}
+		isRepeateCalculation := checkRepeatCalculation()
+		if !isRepeateCalculation{
+			break
+		}
+	}
 }
 
-func getUserInput() (float64) {
-	var userInput float64
-	fmt.Print("Введите ваше значение")
-	fmt.Scan(&userInput)
-	return userInput
+func getUserInput() (float64, string) {
+	var amount float64
+	var currency string
+	fmt.Print("Введите ваше значение: ")
+	fmt.Scan(&amount)
+	fmt.Print("Введите вашу валюту: ")
+	fmt.Scan(&currency)
+	return amount, currency
 }
 
-func calculatUserInfo(amount float64, fromCurrency string, toCurrency string) (result float64) {
+// func calculatUserInfo(amount float64, fromCurrency string, toCurrency string) (result float64) {
 
+// }
+
+func checkRepeatCalculation() (bool) {
+	var userChoise string
+	fmt.Print("Вы хотите сделать еще расчёт (y/n)")
+	fmt.Scan(&userChoise)
+	if userChoise == "y" || userChoise == "Y" {
+		return true
+	}
+	return false
+}
+
+
+func checkError (amount float64, currency string) (error) {
+	if amount <= 0 || currency == "" {
+		return errors.New("")
+	}
+	return nil
 }
