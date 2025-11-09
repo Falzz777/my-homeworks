a730c853039e294b257c497d4e55488f36b69d90
Falzz777 <falzz@yandex.ru>
2025-11-09 22:53:02 +0300

    Add homework 3.1

--- homework-2/main.go
+++ homework-2/main.go
@@ -7,12 +7,14 @@ import (
 
 func main() {
 	for {
-		amount, currency := getUserInput()
-		err := checkError(amount, currency)
+		amount, fromCurrency, toCurrency := getUserInput()
+		err := checkError(amount, fromCurrency, toCurrency)
 		if err != nil {
 			fmt.Println("Введите правильные значения!!!")
 			continue
 		}
+		result := calculatUserInfo(amount, fromCurrency, toCurrency)
+		fmt.Printf("%.1f %s = %.1f %s\n", amount, fromCurrency, result, toCurrency)
 		isRepeateCalculation := checkRepeatCalculation()
 		if !isRepeateCalculation{
 			break
@@ -20,19 +22,35 @@ func main() {
 	}
 }
 
-func getUserInput() (float64, string) {
+func getUserInput() (float64, string,  string) {
 	var amount float64
-	var currency string
+	var fromCurrency string
+	var toCurrency string
+	fmt.Print("Введите вашу исходную валюту(USD, EUR, RUB): ")
+	fmt.Scan(&fromCurrency)
 	fmt.Print("Введите ваше значение: ")
 	fmt.Scan(&amount)
-	fmt.Print("Введите вашу валюту: ")
-	fmt.Scan(&currency)
-	return amount, currency
+	fmt.Print("Введите вашу целевую валюту(USD, EUR, RUB): ")
+	fmt.Scan(&toCurrency)
+	return amount, fromCurrency, toCurrency
 }
 
-// func calculatUserInfo(amount float64, fromCurrency string, toCurrency string) (result float64) {
-
-// }
+func calculatUserInfo(amount float64, fromCurrency string, toCurrency string) (float64) {
+	switch {
+		case fromCurrency == "USD" && toCurrency == "EUR":
+			return amount * 0.85
+		case fromCurrency == "EUR" && toCurrency == "USD":
+			return amount * 1.18
+		case fromCurrency == "RUB" && toCurrency == "USD":
+			return amount * 0.012
+		case fromCurrency == "RUB" && toCurrency == "EUR":
+			return amount * 0.011
+		case fromCurrency == "EUR" && toCurrency == "RUB":
+			return amount * 93.69
+		default:
+			return amount * 81.01
+ 	}
+}
 
 func checkRepeatCalculation() (bool) {
 	var userChoise string
@@ -45,8 +63,8 @@ func checkRepeatCalculation() (bool) {
 }
 
 
-func checkError (amount float64, currency string) (error) {
-	if amount <= 0 || currency == "" {
+func checkError (amount float64, fromCurrency string, toCurrency string) (error) {
+	if amount <= 0 || fromCurrency == "" || toCurrency == ""{
 		return errors.New("")
 	}
 	return nil
